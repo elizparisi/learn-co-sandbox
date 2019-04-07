4a533ad20602478f1a809c1dee7a72304ef70b40
Elizabeth Parisi <parisielizabeth@gmail.com>
2019-04-07 19:56:52 +0000

    first commit

---
+++ man-booker-prize-cli/lib/man/booker/prize/cli.rb
@@ -0,0 +1,12 @@
+require "man/booker/prize/cli/version"
+
+module Man
+  module Booker
+    module Prize
+      module Cli
+        class Error < StandardError; end
+        # Your code goes here...
+      end
+    end
+  end
+end
